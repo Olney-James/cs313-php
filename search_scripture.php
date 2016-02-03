c5b98a6aeef85b0d468a4279c71012549f933341
James Olney <jamesolney1@gmail.com>
2016-02-03 10:07:07 -0700

    changed back to localhost. am I insane?

--- search_scripture.php
+++ search_scripture.php
@@ -1,10 +1,10 @@
 <?php
 function test() {
-	$server  = '127.0.0.1';
+	$server  = 'localhost';
 	$database = 'scriptures';
 	$username = 'php';
 	$password = 'php-pass';
-	$dsn = 'mysql:host='.$server.';dbname='.$database.';port=8080';
+	$dsn = 'mysql:host='.$server.';dbname='.$database;
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 
 	try{
