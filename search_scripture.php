3b9e11698116e1f0d3edd19c09635cde0dd7a02f
James Olney <jamesolney1@gmail.com>
2016-02-03 12:50:56 -0700

    commented out extra echo used for testing

--- search_scripture.php
+++ search_scripture.php
@@ -18,13 +18,13 @@ function test() {
 	} 
 	
 }
-/* test stuff */
+/* test stuff 
 	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
-	/* test stuff */
+	 test stuff */
 
 $test = test();
 
