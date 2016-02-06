fd5fe7ae90fbd625949850ccfe593ef4710f4f18
James Olney <jamesolney1@gmail.com>
2016-02-06 06:15:00 -0700

    adding

---
+++ whybuy.php
@@ -0,0 +1,56 @@
+<HTML>
+   <HEAD>
+	   <style>
+			body {
+				background-color: #d0e4fe;
+			}
+
+			h1 {
+				color: red;
+				text-align: center;
+				font-family: "Magneto"
+			}
+			
+			h3 {
+				color:whitesmoke;
+				text-align:center;
+			}
+			div.img {
+				background-color: red;
+				height: 160px;
+				width: 160px;
+				box-shadow: 5px 5px 5px black;
+				float: left;
+				position:relative;
+				color: whitesmoke;
+				font-size: 24;
+			}
+			div.assignments {
+				background-color: red;
+				height: 160px;
+				width: 160px;
+				box-shadow: 5px 5px 5px black;
+				float: right;
+				position:relative;
+				color: whitesmoke;
+				font-size: 24;
+			}
+			div.container {
+				height: 200px;
+				width: 540px;
+				position:relative;
+			}
+		</style>
+		
+      <TITLE>
+         Assignments
+      </TITLE>
+   </HEAD>
+<BODY>
+   <H1>WhyBuy</H1>
+
+   <a href="index.html">
+		<h3>Home</h3>
+	</a>
+</BODY>
+</HTML>
