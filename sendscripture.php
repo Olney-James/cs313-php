b4f9923b627a3a62787e89615311a530d3d30861
James Olney <jamesolney1@gmail.com>
2016-02-13 16:39:08 -0700

    display all scriptures

--- sendscripture.php
+++ sendscripture.php
@@ -35,6 +35,33 @@
 	// Begin a new Transaction -->
 	$test->beginTransaction();
 	
+	function viewScriptures() {
+	global $test;
+	$query = 'SELECT scriptures.scripture_id, scriptures.chapter, scriptures.verse, scriptures.content, books.name
+	FROM scriptures
+	INNER JOIN books
+	ON scriptures.book_id = books.book_id
+	ORDER BY books.name';
+	$statement = $test->prepare($query);
+	$statement->execute();
+	$scriptures = $statement->fetchAll();
+	$statement->closeCursor();
+	return $scriptures;
+	}
+
+	
+	function findTopicByScripture($scripture){
+		global $test;
+		$query = 'SELECT link.topic_id, topics.topic_name FROM link
+		INNER JOIN topics
+		ON link.topic_id = topics.topic_id
+		WHERE link.scripture_id ='. $scripture;
+		$statement = $test->prepare($query);
+		$statement->execute();
+		$topics = $statement->fetchAll();
+		$statement->closeCursor();
+		return $topics;
+	}
 	// First insert the scripture -->
 	$query = '	INSERT INTO scriptures
 						(book_id, chapter, verse, content)
@@ -79,6 +106,57 @@ insertScripture();
 		</TITLE>
 	</HEAD>
 	<BODY>
+	<!-- below here is the tabs set up -->
+		<div class="container">
+		  <h2>Items</h2>
+		  <ul class="nav nav-tabs">
+			<li class="active"><a data-toggle="tab" href="#home">All</a></li>
+			<li><a data-toggle="tab" href="#menu1">Books</a></li>
+			<li><a data-toggle="tab" href="#menu2">Games</a></li>
+			<li><a data-toggle="tab" href="#menu3">Gadgets</a></li>
+		  </ul>
+
+		  <div class="tab-content">
+			<div id="home" class="tab-pane fade in active">
+			  <h3>All Scriptures</h3>
+				  <table id="t01">
+						<tr>
+							<th>Book</th>
+							<th>Chapter:Verse</th>
+							<th>Content</th>
+							<th>Topics</th>
+						  </tr>
+						<?php 	$scriptures=viewScriptures(); 
+								foreach ($scriptures as $scripture): 
+						?>
+							<tr>
+								
+								<td><?php echo $scripture["book"]; ?></td>
+								<td><?php echo $scripture["chapter"]; ?>:<?php echo $scripture["verse"]; ?></td>
+								<td><?php echo $scripture["content"]; ?></td>							
+						<?php endforeach; ?>
+								<td><?php 
+								$topics=findTopicByScripture($scripture["scripture_id"]);
+								foreach($topics as $topic){
+									echo $topic["topic_name"] . " "; 
+								}
+									?></td>
+							</tr>
+					</table>
+			</div>
+			<div id="menu1" class="tab-pane fade">
+			  <h3>Charity</h3>
+			</div>
+			<div id="menu2" class="tab-pane fade">
+				<h3>Faith</h3>
+			</div>
+			<div id="menu3" class="tab-pane fade">
+			  <h3>Sacrifice</h3>
+			  
+			</div>
+		  </div>
+		</div>
+	<!-- above here is the tabs set up -->
 		<center>
 			<a href="search_scripture.php"><h3>View Scriptures</h3></a>
 			<a href="scriptureInsert.php"><h3>Add a scripture</h3></a>
