7d966e7cc384cb3f1e6771f269b39eed8f8654b8
James Olney <jamesolney1@gmail.com>
2016-02-13 20:17:03 -0700

    connected to database

--- loginsubmit.php
+++ loginsubmit.php
@@ -1,4 +1,22 @@
-<?php 
+<?php
+	function test() {
+		$server  = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$database = 'retail_site';
+		$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+		$dsn = 'mysql:host='.$server.';dbname='.$database;
+
+		try{
+			$g1db = new PDO($dsn, $username, $password);
+			return $g1db;	
+		}
+		catch (PDOException $ex){
+			echo 'Error!:' . $ex->getMessage();
+			die();
+		} 	
+	}	
+	$test = test();
+	
 	function selectUsers($user){
 		global $test;
 		$query = 'SELECT * FROM user_name
