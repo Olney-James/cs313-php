42ca8136936f0f82ee9085637f0b396ab336156f
James Olney <jamesolney1@gmail.com>
2016-02-15 10:26:42 -0700

    deleting more code

--- addToCart.php
+++ addToCart.php
@@ -1,25 +1,4 @@
 <?php 
-	session_start();
-	function test() {
-		$server  = getenv('OPENSHIFT_MYSQL_DB_HOST');
-		$database = 'retail_site';
-		$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-		$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-		$dsn = 'mysql:host='.$server.';dbname='.$database;
-
-		try{
-			$g1db = new PDO($dsn, $username, $password);
-			return $g1db;
-			
-		}
-		catch (PDOException $ex){
-			echo 'Error!:' . $ex->getMessage();
-			die();
-		} 
-		
-	}
-		
-	$test = test();
 /*below are shopping-cart functions */
 	function addToCart($item_id) {
 		global $test;
